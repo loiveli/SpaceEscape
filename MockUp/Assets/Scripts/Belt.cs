87d3c0e5cee3a47ac9443c3e6e1e44b19bb874ce
Antti <antti.vornanen@gmail.com>
2018-10-22 14:36:37 +0300

    treadmill first version with rigidbody

---
+++ MockUp/Assets/Scripts/Belt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Belt : MonoBehaviour {
+
+	[SerializeField]
+	float speed;
+
+	private float currentScroll;
+
+    
+	private void Update()
+    {
+
+		//Texture Scroller (Main texture)
+		currentScroll = currentScroll + Time.deltaTime * speed;
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, currentScroll);
+    }
+
+    
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.position -= transform.forward * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + transform.forward * speed * Time.deltaTime);
+    }
+
+}
+
+
