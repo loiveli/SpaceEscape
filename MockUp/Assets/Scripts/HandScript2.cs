6775e30730bc72695d19fa083d7b8de6bfd38318
Unknown <valtteri.nietula@gmail.com>
2018-12-06 18:30:38 +0200

    New hand script

---
+++ MockUp/Assets/Scripts/HandScript2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScript2 : MonoBehaviour {
+	public Vector3 targetPos;
+	public Vector3 startPos;
+	public GameObject spawner;
+	public bool deliver;
+	// Use this for initialization
+	int powerUpLane;
+	void Start () {
+		deliver = false;
+	}
+	public void StartDelivery(Transform refPlane, int xScale){
+		targetPos = PlayerMover.GetPosInPlane((xScale*0.25f)+0.25f, 1,1,refPlane);
+		deliver = true;
+		powerUpLane =xScale;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(deliver){
+			transform.position = Vector3.MoveTowards(transform.position, targetPos, 0.5f);
+
+		}else if(transform.position != startPos){
+			transform.position = Vector3.MoveTowards(transform.position, startPos, 1f);
+		}
+		if(transform.position == targetPos){
+			SpawnPowerUp();
+			
+		}
+	}
+	void SpawnPowerUp(){
+		spawner.GetComponent<spawner>().powerUp = true;
+		spawner.GetComponent<spawner>().powerUpLane = powerUpLane-2;
+	}
+	
+}
